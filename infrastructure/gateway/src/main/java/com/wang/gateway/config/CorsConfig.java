53cf06bfddb11c46d155cb403f12b1a9ee292f43
wangtingyun <wty1793172997@163.com>
2020-08-29 13:00:49 +0800

    添加Canal和Gateway模块

---
+++ infrastructure/gateway/src/main/java/com/wang/gateway/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.wang.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * <p>
+ * 处理跨域
+ * </p>
+ *
+ * @author 王廷云
+ * @since 2020-08-28
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
+}
---
+++ infrastructure/gateway/src/main/java/com/wang/gateway/handler/ErrorHandlerConfig.java
@@ -0,0 +1,4 @@
+package com.wang.gateway.handler;
+
+public class ErrorHandlerConfig {
+}
